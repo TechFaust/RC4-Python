e71147720c2640e02662ed4a4f7552d070bbe77c
hearthdragon <jean-benoit.duflot@isen.yncrea.fr>
2024-10-08 19:24:14 +0200

    Sounds like a skill issue to me Jean-Benoit

--- display.py
+++ display.py
@@ -1,15 +1,130 @@
-# Fonction à appeler
-def DrawEverything() -> None:
-    pass
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 
-# NE PAS TOUCHER
-def __DrawMobile() -> None:
-    pass
+from Milieu import Milieu
+from constants import ESPACE_TRAVAIL as ET, Position, RAYON_POULIE as RP, POSITIONS_POULIES as PP
+from math import pi
 
-# NE PAS TOUCHER
-def __DrawPoulies() -> None:
-    pass
+class Display:
+    def __init__(self, milieu: Milieu):
+        self.__milieu = milieu
 
-# NE PAS TOUCHER
-def __DrawCables() -> None:
-    pass
+        self.__root = tk.Tk()
+        self.__root.title("RC4 Controller")
+
+        self.__mainframe = ttk.Frame(self.__root, padding="10 10 10 10")
+        self.__mainframe.grid(column=0, row=0, columnspan=2)
+
+        self.__x = tk.StringVar()
+        self.__x.trace('w', lambda index, name, hop: self.submit())
+        self.__y = tk.StringVar()
+        self.__y.trace('w', lambda index, name, hop: self.submit())
+        self.__theta = tk.StringVar()
+        self.__theta.trace('w', lambda index, name, hop: self.submit())
+
+        ttk.Label(self.__mainframe, text="X:").grid(column=1, row=1, sticky=tk.W)
+        x_entry = ttk.Entry(self.__mainframe, width=7, textvariable=self.__x)
+        x_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+
+        ttk.Label(self.__mainframe, text="Y:").grid(column=1, row=2, sticky=tk.W)
+        y_entry = ttk.Entry(self.__mainframe, width=7, textvariable=self.__y)
+        y_entry.grid(column=2, row=2, sticky=(tk.W, tk.E))
+
+        ttk.Label(self.__mainframe, text="Theta:").grid(column=1, row=3, sticky=tk.W)
+        theta_entry = ttk.Entry(self.__mainframe, width=7, textvariable=self.__theta)
+        theta_entry.grid(column=2, row=3, sticky=(tk.W, tk.E))
+
+        self.__distance_labels = {
+            "BG": ttk.Label(self.__mainframe, text=""),
+            "BD": ttk.Label(self.__mainframe, text=""),
+            "HG": ttk.Label(self.__mainframe, text=""),
+            "HD": ttk.Label(self.__mainframe, text="")
+        }
+
+        self.__distance_labels["BG"].grid(column=1, row=5, columnspan=2, sticky=tk.W)
+        self.__distance_labels["BD"].grid(column=1, row=6, columnspan=2, sticky=tk.W)
+        self.__distance_labels["HG"].grid(column=1, row=7, columnspan=2, sticky=tk.W)
+        self.__distance_labels["HD"].grid(column=1, row=8, columnspan=2, sticky=tk.W)
+
+        self.__figure = Figure(figsize=(5, 4), dpi=100)
+        self.__canvas = FigureCanvasTkAgg(self.__figure, master=self.__root)
+        self.__canvas.get_tk_widget().grid(column=3, row=0, rowspan=5, padx=10, pady=10)
+
+        for child in self.__mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+    def start(self) -> None:
+        self.__root.mainloop()
+
+    def submit(self) -> None:
+        x = float()
+        y = float()
+        theta = float()
+        try:
+            x = float(self.__x.get())
+            y = float(self.__y.get())
+            theta = float(self.__theta.get())
+        except ValueError:
+            x = ET[0] / 2
+            y = ET[1] / 2
+            theta = 0
+        finally:
+            self.__milieu.move((x, y))
+            self.__milieu.turn(theta * pi / 180)
+            self.__distance_labels["BG"].config(
+                text=f"distance bas gauche = {"{:10.4f}".format(self.__milieu.l_cable(Position.BG))}mm")
+            self.__distance_labels["BD"].config(
+                text=f"distance bas droite = {"{:10.4f}".format(self.__milieu.l_cable(Position.BD))}mm")
+            self.__distance_labels["HG"].config(
+                text=f"distance haut gauche = {"{:10.4f}".format(self.__milieu.l_cable(Position.HG))}mm")
+            self.__distance_labels["HD"].config(
+                text=f"distance haut droite = {"{:10.4f}".format(self.__milieu.l_cable(Position.HD))}mm")
+
+            self.show(
+                self.__milieu.angle_mobile(Position.BG),
+                self.__milieu.angle_mobile(Position.BD),
+                self.__milieu.angle_mobile(Position.HD),
+                self.__milieu.angle_mobile(Position.HG)
+            )
+
+    def show(self, bg: (float, float), bd: (float, float), hd: (float, float), hg: (float, float)) -> None:
+        self.__figure.clear()
+
+        # Rectangle coordinates
+        x = [bg[0], bd[0], hd[0], hg[0], bg[0]]
+        y = [bg[1], bd[1], hd[1], hg[1], bg[1]]
+
+        # Plot the rectangle
+        ax = self.__figure.add_subplot(111)
+        ax.plot(x, y, marker='o')
+        ax.fill(x, y, alpha=0.3)  # Fill the rectangle with a color
+
+        # Plot the pulleys
+        poulies = [PP['BG'], PP['BD'], PP['HD'], PP['HG']]
+        for (px, py) in poulies:
+            circle = plt.Circle((px, py), RP, color='r', fill=False)
+            ax.add_patch(circle)
+
+        # Plot the cables
+        corners = [bg, bd, hd, hg]
+        for (cx, cy), (px, py) in zip(corners, poulies):
+            ax.plot([cx, px], [cy, py], 'k--')  # Dashed line for the cable
+
+        # Set the limits of the plot
+        ax.set_xlim(-200, 700)
+        ax.set_ylim(-200, 800)
+
+        # Graph parameters
+        ax.set_title('RC4 Déplacement')
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.grid()
+
+        # Set aspect ratio
+        ax.set_aspect('equal', adjustable='box')
+
+        # Draw the canvas
+        self.__canvas.draw()
--- main.py
+++ main.py
@@ -1,117 +1,11 @@
-import tkinter as tk
-from tkinter import ttk
 from Milieu import Milieu
 from Mobile import Mobile
-from constants import Position, RAYON_POULIE
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-
-def show(canvas, figure, x1, y1, x2, y2, x3, y3, x4, y4,milieu):
-    figure.clear()
-
-    # Poulies
-    poulies = [(-175, -175), (675, -175), (675, 775), (-175, 775)]
-
-    # Rectangle coordinates
-    x = [x1, x2, x3, x4, x1]
-    y = [y1, y2, y3, y4, y1]
-
-    # Plot the rectangle
-    ax = figure.add_subplot(111)
-    ax.plot(x, y, marker='o')
-    ax.fill(x, y, alpha=0.3)  # Fill the rectangle with a color
-
-    # Plot the pulleys
-    for (px, py) in poulies:
-        circle = plt.Circle((px, py), RAYON_POULIE, color='r', fill=False)
-        ax.add_patch(circle)
-
-    # Plot the cables
-    corners = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
-    for (cx, cy), (px, py) in zip(corners, poulies):
-        ax.plot([cx, px], [cy, py], 'k--')  # Dashed line for the cable
-
-
-    # Set the limits of the plot
-    ax.set_xlim(-200, 700)
-    ax.set_ylim(-200, 800)
-
-    # Graph parameters
-    ax.set_title('RC4 Déplacement')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.grid()
-
-    # Set aspect ratio
-    ax.set_aspect('equal', adjustable='box')
-
-    # Draw the canvas
-    canvas.draw()
-
-
+from display import Display
 
 def main() -> None:
     mob = Mobile(100, 50, (100, 100), 0)
     milieu = Milieu(mob)
-
-    milieu.move((0, 0))
-
-    root = tk.Tk()
-    root.title("RC4 Controller")
-
-    mainframe = ttk.Frame(root, padding="10 10 10 10")
-    mainframe.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-
-    ttk.Label(mainframe, text="X:").grid(column=1, row=1, sticky=tk.W)
-    x_entry = ttk.Entry(mainframe, width=7)
-    x_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
-
-    ttk.Label(mainframe, text="Y:").grid(column=1, row=2, sticky=tk.W)
-    y_entry = ttk.Entry(mainframe, width=7)
-    y_entry.grid(column=2, row=2, sticky=(tk.W, tk.E))
-
-    ttk.Label(mainframe, text="Theta:").grid(column=1, row=3, sticky=tk.W)
-    theta_entry = ttk.Entry(mainframe, width=7)
-    theta_entry.grid(column=2, row=3, sticky=(tk.W, tk.E))
-
-    figure = Figure(figsize=(5, 4), dpi=100)
-    canvas = FigureCanvasTkAgg(figure, master=root)
-    canvas.get_tk_widget().grid(column=3, row=0, rowspan=5, padx=10, pady=10)
-
-    distance_labels = {
-        "BG": ttk.Label(mainframe, text=""),
-        "BD": ttk.Label(mainframe, text=""),
-        "HG": ttk.Label(mainframe, text=""),
-        "HD": ttk.Label(mainframe, text="")
-    }
-
-    distance_labels["BG"].grid(column=1, row=5, columnspan=2, sticky=tk.W)
-    distance_labels["BD"].grid(column=1, row=6, columnspan=2, sticky=tk.W)
-    distance_labels["HG"].grid(column=1, row=7, columnspan=2, sticky=tk.W)
-    distance_labels["HD"].grid(column=1, row=8, columnspan=2, sticky=tk.W)
-
-    def on_submit():
-        x = float(x_entry.get())
-        y = float(y_entry.get())
-        theta = float(theta_entry.get())
-
-        milieu.move((x, y))
-        milieu.turn(theta)
-
-        show(canvas,figure,mob.BG[0], mob.BG[1], mob.BD[0], mob.BD[1], mob.HD[0], mob.HD[1], mob.HG[0], mob.HG[1],milieu)
-
-        distance_labels["BG"].config(text=f"distance bas gauche = {milieu.l_cable(Position.BG)}mm")
-        distance_labels["BD"].config(text=f"distance bas droite = {milieu.l_cable(Position.BD)}mm")
-        distance_labels["HG"].config(text=f"distance haut gauche = {milieu.l_cable(Position.HG)}mm")
-        distance_labels["HD"].config(text=f"distance haut droite = {milieu.l_cable(Position.HD)}mm")
-
-    ttk.Button(mainframe, text="Submit", command=lambda: on_submit()).grid(column=2, row=4, sticky=tk.W)
-
-    for child in mainframe.winfo_children():
-        child.grid_configure(padx=5, pady=5)
-
-    root.mainloop()
+    Display(milieu).start()
 
     exit(0)
 
